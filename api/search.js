09b2bcf4e1e2aa2c29e82954dddae37c6514ebdb
Aldi Alhafidzi <aldi.alhafidzi@gmail.com>
2026-01-20 00:09:56 +0700

    add page query params

--- api/search.js
+++ api/search.js
@@ -1,12 +1,13 @@
 export default async function handler(req, res) {
   const q = req.query.q || "example";
+  const page = req.query.page || 1;
   const max = req.query.max || 5;
   const API_KEY = process.env.GNEWS_API_KEY;
 
   try {
     const url = `https://gnews.io/api/v4/search?q=${encodeURIComponent(
       q,
-    )}&max=${max}&lang=en&apikey=${API_KEY}`;
+    )}&page=${page}&max=${max}&lang=en&apikey=${API_KEY}`;
 
     const response = await fetch(url);
     const data = await response.json();
